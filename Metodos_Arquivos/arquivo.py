106b8c38e7a79ecd3e47ce72c2b852513ff6fa98
Gabriel Maia <gabrielmaia6743@gmail.com>
2025-03-29 11:23:01 -0300

    Leitura e Salvamento de dados feito

--- Metodos_Arquivos/arquivo.py
+++ Metodos_Arquivos/arquivo.py
@@ -1,9 +1,11 @@
 # Aqui estaram os códigos de ler arquivo e salvar os dados em arquivo
+import numpy as np
 
 # Função para ler dados do dataset
-def LerArquivo():
-    pass
+def LerArquivo(nome):
+    instancia = np.loadtxt(f"Datasets/{nome}.txt", dtype=int)
+    return instancia
 
 # Função para salvar dados que tiramos após operações com o dataset
-def SalvarArquivo():
-    pass
+def SalvarArquivo(dados, nome):
+    return np.savetxt(f"Salvar Dados/{nome}.txt", dados, fmt="%d")
