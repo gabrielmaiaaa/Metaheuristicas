2300605035143c86cf4b2530b1681d3bfe8b6022
Gabriel Maia <gabrielmaia6743@gmail.com>
2025-03-29 11:21:16 -0300

    Base dos arquivos feito

---
+++ Metodos_Arquivos/arquivo.py
@@ -0,0 +1,9 @@
+# Aqui estaram os códigos de ler arquivo e salvar os dados em arquivo
+
+# Função para ler dados do dataset
+def LerArquivo():
+    pass
+
+# Função para salvar dados que tiramos após operações com o dataset
+def SalvarArquivo():
+    pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from Metodos_Arquivos.arquivo import *
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
